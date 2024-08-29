21c9a4f079afea19ff3b53bb7ad1668f0fd33c0c
tejaswinidg <55744552+tejaswinidg@users.noreply.github.com>
2024-08-29 17:26:01 +0530

    Update aa.java

--- src/test/java/aa.java
+++ src/test/java/aa.java
@@ -48,35 +48,35 @@ public class aa {
 	  System.out.println("title of window1");
 	  System.out.println(f);
 	  
-System.out.println("fffffffffff");
+
 	
 
 	  
-//	  try {
-//		
-// driver.findElement(By.xpath("//h2[text()='TYPES OF MANUAL']"));
-//System.out.println("l");
-//
-//JavascriptExecutor js=(JavascriptExecutor) driver;
-//                              
-//js.executeScript("arguments[0].click();");
-//	  List<WebElement> li=driver.findElements(By.xpath("//div [@class=\"sideBar--wrap newLeftbar\"]//h2[text()='TYPES OF MANUAL']/following-sibling::ul"));
-//	 
-//	  for(WebElement g:li) {
-//		  if(g.getText().contains("White box Testing - Software Engineering")) {
-//			  g.click();
-//			  
-//		  }
-//	  }
+	  try {
+		
+driver.findElement(By.xpath("//h2[text()='TYPES OF MANUAL']"));
+System.out.println("l");
 
+JavascriptExecutor js=(JavascriptExecutor) driver;
+                             
+js.executeScript("arguments[0].click();");
+	  List<WebElement> li=driver.findElements(By.xpath("//div [@class=\"sideBar--wrap newLeftbar\"]//h2[text()='TYPES OF MANUAL']/following-sibling::ul"));
+	 
+	  for(WebElement g:li) {
+		  if(g.getText().contains("White box Testing - Software Engineering")) {
+			  g.click();
+			  
+		  }
+	  }
+
+	  }
+//	  catch(Exception e){
+//			String FileName="iiiI";
+//			// Creating instance of File
+//			File File = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+//	
+//			FileUtils.copyFile(File, new File("C:\\Users\\Tejaswini\\Desktop\\dd\\" + FileName + ".jpeg"));
 //	  }
-////	  catch(Exception e){
-////			String FileName="iiiI";
-////			// Creating instance of File
-////			File File = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-////	
-////			FileUtils.copyFile(File, new File("C:\\Users\\Tejaswini\\Desktop\\dd\\" + FileName + ".jpeg"));
-////	  }
 	 
 //	 long start = System.currentTimeMillis();
 //	 
