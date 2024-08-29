f88f6a4696715be533df3f5427df3b43b7bf2ab6
tejaswinidg <55744552+tejaswinidg@users.noreply.github.com>
2024-08-29 14:27:34 +0530

    Update aa.java

--- src/test/java/aa.java
+++ src/test/java/aa.java
@@ -37,14 +37,14 @@ public class aa {
 	  driver.findElement(By.xpath("//input[@name='username']")).sendKeys("jk");
 	 
 	
-//	  driver.findElement(By.xpath("//*[text()='Admin Page']")).click();
-//	String h=  driver.getTitle();
-//	System.out.println("title of the window2");
-//	  System.out.println(h);
-//	  driver.navigate().back();
-//	  String f=driver.getTitle();
-//	  System.out.println("title of window1");
-//	  System.out.println(f);
+	  driver.findElement(By.xpath("//*[text()='Admin Page']")).click();
+	String h=  driver.getTitle();
+	System.out.println("title of the window2");
+	  System.out.println(h);
+	  driver.navigate().back();
+	  String f=driver.getTitle();
+	  System.out.println("title of window1");
+	  System.out.println(f);
 	  
 	  
 	  
