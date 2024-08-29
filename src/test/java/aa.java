846dd6e2ffcf29f1881bbabe4dd10640b4c82505
Tejaswini DG <tejaswinidg#gmail.com>
2024-08-29 16:57:02 +0530

    oppg

--- src/test/java/aa.java
+++ src/test/java/aa.java
@@ -36,7 +36,8 @@ public class aa {
 	  driver.get("https://parabank.parasoft.com/parabank/register.htm");
 	  driver.findElement(By.xpath("//input[@name='username']")).sendKeys("jk");
 	 
-	
+	System.out.println("hhhhhhhhh");
+	System.out.println("klllllll");
 	
 	  driver.findElement(By.xpath("//*[text()='Admin Page']")).click();
 	String h=  driver.getTitle();
@@ -47,7 +48,7 @@ public class aa {
 	  System.out.println("title of window1");
 	  System.out.println(f);
 	  
-	System.out.println("io");  
+	 
 	  
 //	  try {
 //		
